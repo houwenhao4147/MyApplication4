ccb753ad14b56d09ac6855a68ca277427d900c79
houwenhao4147 <houwenhao4147@163.com>
2017-07-25 23:37:46 +0800

    dev 3

--- app/src/main/java/a20170802gitdemo/myapplication/MainActivity.java
+++ app/src/main/java/a20170802gitdemo/myapplication/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("TAG","dev");
-        Log.e("TAG","dev2");
-        Log.e("TAG","dev3");
+        Log.e("TAG","bug");
     }
 }
